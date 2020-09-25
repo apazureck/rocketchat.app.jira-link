e73143103af8074f0a99acf514769f3beb5a97dc
Andreas Pazureck <andreas@pazureck.de>
2020-09-25 09:59:09 +0200

    added unittest for happy path

--- test/jiraIssueProviderTests.ts
+++ test/jiraIssueProviderTests.ts
@@ -1,25 +1,47 @@
 import { ILogger } from "@rocket.chat/apps-engine/definition/accessors";
 import { expect } from "chai";
 import "mocha";
-import { anyString, anything, mock, when } from "ts-mockito";
-import { It, Mock } from "typemoq";
+import { Mock } from "typemoq";
 // tslint:disable:no-unused-expression
 
 import { JiraConnection } from "../src/jiraConnection/jiraConnection";
 import { IJiraIssue, ISSUE_URL_PREFIX, JiraIssueProvider } from "../src/jiraConnection/jiraIssueProvider";
 
 describe("Jira Issue Provider Tests", () => {
-    it("Fail should return undefined", async () => {
+    it("Failed request should return undefined", async () => {
+        // Arrange
         const issueId = "PRJ-123";
         const requestUrl = ISSUE_URL_PREFIX + issueId;
-        const logger: ILogger = mock<ILogger>();
+        const logger = Mock.ofType<ILogger>();
         const jiraConnectionMock = Mock.ofType<JiraConnection>();
-        jiraConnectionMock.setup(jc => jc.request(It.isAny())).throws(new Error());
+        jiraConnectionMock.setup(jc => jc.request(requestUrl)).throws(new Error());
 
-        const cut = new JiraIssueProvider(jiraConnectionMock.object, logger);
+        const cut = new JiraIssueProvider(jiraConnectionMock.object, logger.object);
+
+        // Act
 
         const result = await cut.getIssue(issueId);
 
+        // Assert
         expect(result).to.be.undefined;
+    });
+
+    it("Valid request should return issue", async () => {
+        // Arrange
+        const issueId = "PRJ-123";
+        const requestUrl = ISSUE_URL_PREFIX + issueId;
+        const logger = Mock.ofType<ILogger>();
+        const jiraConnectionMock = Mock.ofType<JiraConnection>();
+        jiraConnectionMock.setup(jc => jc.request<IJiraIssue>(requestUrl))
+            .returns(async () => {
+                return { key: issueId } as IJiraIssue;
+            });
+
+        const cut = new JiraIssueProvider(jiraConnectionMock.object, logger.object);
+
+        // Act
+        const result = await cut.getIssue(issueId) as IJiraIssue;
+
+        expect(result.key).to.equal(issueId);
     });
 });
