6d018e2af16d15aeb50fbbd1dae885d63c305544
Andreas Pazureck <andreas@pazureck.de>
2020-10-03 22:19:54 +0200

    Added tests for issue replacer

--- src/messageHandlers/domain/issueReplacer.ts
+++ src/messageHandlers/domain/issueReplacer.ts
@@ -6,13 +6,14 @@ export class IssueReplacer implements IIssueReplacer {
     public callback: (issue: IJiraIssue) => string = issue => `[${issue.key}](${issue.jiraLinkBrowseAddress})`;
 
     public replaceIssues(foundIssues: Array<IFoundIssue>, text: string): string {
-        for (const issue of foundIssues.map(fi => fi.issue)) {
-            text = text.replace(
-                issue.issueId,
-                this.callback(issue)
-            );
+
+        for (const foundIssue of foundIssues.reverse()) {
+            text = replaceInTextByIndexAndLength(text, this.callback(foundIssue.issue), foundIssue.foundMatch.index, foundIssue.foundMatch[0].length);
         }
         return text;
     }
+}
 
+export function replaceInTextByIndexAndLength(text: string, replacement: string, start: number, length: number): string {
+    return text.replace(new RegExp("^(.{" + start + "}).{" + length + "}"), "$1" + replacement);
 }
--- test/messageHandlers/domain/jiraIssueReplacer.spec.ts
+++ test/messageHandlers/domain/jiraIssueReplacer.spec.ts
@@ -1,41 +1,104 @@
+// tslint:disable:no-unused-expression
+
 import { expect } from "chai";
 import "mocha";
 import { IJiraIssue } from "../../../src/definition/jiraConnection";
-import { IFoundIssue, IJiraIssueMessageParser } from "../../../src/definition/messageHandling";
-import { IssueReplacer } from "../../../src/messageHandlers/domain/issueReplacer";
+import {
+    IFoundIssue,
+} from "../../../src/definition/messageHandling";
+import { IssueReplacer, replaceInTextByIndexAndLength } from "../../../src/messageHandlers/domain/issueReplacer";
 
 describe("Tests for issue replacement", () => {
     it("Correct Issue should be replaced", () => {
         // Arrange
-
-        let text = "ISSUE-123 needs to be done immediately";
-
-        const foundIissue: IFoundIssue = {
-            issueId: "ISSUE-123",
-            foundMatch: { index: 0, length: 9 } as RegExpExecArray,
-            issue: {
-                key: "ISSUE-123",
-                fields: {
-                    summary: "Summary",
-                    description: "Description",
-                    issuetype: {
-                        name: "IssuetypeName",
-                        iconUrl: "IssuetypeIconurl"
-                    },
-                    status: { name: "statusName" }
-                }
-            } as IJiraIssue
-        };
+        const issueKey = "ISSUE-123";
+        let text = `${issueKey} needs to be done immediately`;
+        const issueRegex = /ISSUE-123(?=\s)/gm;
+        const foundIssues = createFoundIssue({ key: issueKey, match: issueRegex.exec(text) as RegExpExecArray });
 
         const cut = new IssueReplacer();
         cut.callback = (issue: IJiraIssue) => "REPLACED!";
 
         // Act
 
-        text = cut.replaceIssues([foundIissue], text);
+        text = cut.replaceIssues(foundIssues, text);
 
         // Assert
 
-        expect(text.startsWith("REPLACED!"));
+        expect(text.startsWith("REPLACED!")).to.be.equal(true);
+    });
+
+    it("Not recognized issues should not be replaced", () => {
+        // Arrange
+        const issueKey1 = "ISSUE-123";
+        const issueKey2 = "ISSUE-456";
+        const replaced = "REPLACED!";
+        let text = `${issueKey1} needs to be done immediately, [${issueKey1}], ${issueKey2} `;
+        const expectedResultText = `${replaced} needs to be done immediately, [${issueKey1}], ${replaced} `;
+        const issueRegex = /ISSUE-\d+(?=\s)/gm;
+
+        const foundIssues = createFoundIssue({
+            key: issueKey1,
+            match: issueRegex.exec(text) as RegExpExecArray,
+        }, {
+            key: issueKey2,
+            match: issueRegex.exec(text) as RegExpExecArray
+        });
+
+        const cut = new IssueReplacer();
+        cut.callback = (issue: IJiraIssue) => replaced;
+
+        // Act
+
+        text = cut.replaceIssues(foundIssues, text);
+
+        // Assert
+
+        expect(text).to.be.equal(expectedResultText);
+    });
+
+    it("Test Replace function", () => {
+
+        // Arrange
+        const text = "Text should be DONE by this algorithm";
+        const done = "DONE";
+        const replaced = "REPLACED";
+
+        // Act
+
+        const replacedText = replaceInTextByIndexAndLength(text, replaced, text.indexOf(done), done.length);
+
+        // Assert
+
+        expect(replacedText).to.be.equal("Text should be REPLACED by this algorithm");
     });
 });
+
+function createFoundIssue(
+    ...issues: Array<{
+        key: string;
+        match: RegExpExecArray;
+    }>
+): Array<IFoundIssue> {
+    const retarray: Array<IFoundIssue> = [];
+    for (const issue of issues) {
+        retarray.push({
+            issueId: issue.key,
+            foundMatch: issue.match,
+            issue: {
+                key: issue.key,
+                fields: {
+                    summary: "Summary",
+                    description: "Description",
+                    issuetype: {
+                        name: "IssuetypeName",
+                        iconUrl: "IssuetypeIconurl",
+                    },
+                    status: { name: "statusName" },
+                },
+            },
+        });
+    }
+
+    return retarray;
+}
